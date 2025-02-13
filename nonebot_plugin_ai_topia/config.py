dbafddbb630069cdc8b6b96ac3fb201c4f35fd4f
kanzakiD <1690800500@qq.com>
2025-02-13 09:29:15 +0800

    re

--- nonebot_plugin_ai_topia/config.py
+++ nonebot_plugin_ai_topia/config.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel, field_validator
 
 class Config(BaseModel):
     # 配置apikey
-    ai_topia_api_key: str = "topia_d92af2ce76994f"
-    ai_topia_api_secret: str = "a4407c568ff543e08a5151d0b89a55f0"
+    ai_topia_api_key: str = ""
+    ai_topia_api_secret: str = ""
 
     # 角色体ID
-    ai_topia_role_id: str ="2472878"
+    ai_topia_role_id: str =""
 
     # 插件回复优先级：数值越小优先级越高，最小为1
     ai_topia_priority: int = 20
